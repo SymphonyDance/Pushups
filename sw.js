341b0c410065ccb6c08ae371717afe61b9e8075e
SymphonyDance <31779002+SymphonyDance@users.noreply.github.com>
2026-08-26 00:25:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pushups-v8';
+const CACHE = 'pushups-v9';
 const CORE = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'icon-180.png'];
 
 self.addEventListener('install', e => {
